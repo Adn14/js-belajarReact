4beefd011020204a7d5518d600cf069a721939e2
Adn Agung Rochman Arifin <aragungadn1414@gmail.com>
2019-07-22 02:41:35 +0700

    4th commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TodoList from './TodoList';
 //ex : <TodoList/>
 //create App component using class (bawah)
 //using class based, you can add state
-//ashiap
+//ashiap2
 
 class App extends React.Component {
     //create render function (bawah)
